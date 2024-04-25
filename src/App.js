77c634a0dff47f88aa5fc5cc45e299bf41b19853
dangphapqt1997 <dangphapqt1997@gmail.com>
2024-04-25 16:07:20 +0700

    #5 list-host-and-detail-host

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from "./component/Home/Home";
@@ -6,16 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from "./component/login/Login";
 import SignUp from "./component/login/SignUp";
 import RegisterHost from "./component/login/RegisterHost";
+import Host from "./component/host/Host";
+import HostDetail from "./component/host/HostDetail";
+import ListHouse from "./component/host/ListHouse";
 
 function App() {
-  return (
-    <Routes>
-      <Route path={'/'} element={<Home />}></Route>
-      <Route path={'/login'} element={<Login/>}></Route>
-      <Route path={'/register/user'} element={<SignUp/>}></Route>
-      <Route path={'/register/host'} element={<RegisterHost/>}></Route>
-    </Routes>
-  );
+    return (
+
+        <Routes>
+            <Route path={'/'} element={<Home/>}></Route>
+            <Route path={'/login'} element={<Login/>}></Route>
+            <Route path={'/register/user'} element={<SignUp/>}></Route>
+            <Route path={'/register/host'} element={<RegisterHost/>}></Route>
+            <Route path={"/host"} element={<Host/>}></Route>
+            <Route path="/detail/:id" element={<HostDetail/>}></Route>
+            <Route path="/owner/:id" element={<ListHouse/>}></Route>
+        </Routes>
+
+    );
 }
 
 export default App;
